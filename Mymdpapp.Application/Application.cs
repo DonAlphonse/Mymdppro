4dae552627ee8d2218e3d07d5466aef7d6b6b63b
DonAlphonse <70948441+DonAlphonse@users.noreply.github.com>
2023-06-09 17:17:02 +0200

    add the application layer

---
+++ Mymdpapp.Application/Application.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Mymdpapp.Infrastructure;
+
+namespace Mymdpapp.Application
+{
+    public class Application
+    {
+        private List<WebSite> WebSites = new List<WebSite>();
+
+        public void AddWebSite(WebSite webSite)
+        {
+            if (!webSite.Url.StartsWith("www"))
+                throw new System.Exception("Error format");
+            WebSites.Add(webSite);
+        }
+
+        public List<WebSite> GetWebSites()
+        {
+            return WebSites;
+        }
+
+        public void SaveToDatabase()
+        {
+
+        }
+
+        public void RefreshFromDatabase()
+        {
+
+        }
+    }
+}
